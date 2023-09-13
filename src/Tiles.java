314a30e6cdc2a01786d3e81cd61f66ba4d5eb540
Andrey Belozerov <abelozz@yandex.ru>
2023-09-13 11:08:41 +0300

    First commit

--- src/Tiles.java
+++ src/Tiles.java
@@ -2,4 +2,20 @@ import android.graphics.Bitmap;
 
 public class Tiles {
     String zoomLevel, row,column;
+
+          /*         String nameMBTILES, pathFolder;
+        pathFolder = "C:\\Users\\user\\Desktop\\Test_CONVERTER_TMS\\n55e037";
+       nameMBTILES = "n55e037.mbtiles";
+        DataBaseHelper db = new DataBaseHelper(pathFolder,nameMBTILES);
+        MetadataTile metadataTile = db.getZoom();
+       System.out.println(metadataTile.getMaxZoom());
+        int i = Integer.parseInt(metadataTile.getMinZoom());
+        int end = Integer.parseInt(metadataTile.getMaxZoom());
+
+         while (i<=end)
+        {
+            db.convertToWebpFormat(String.valueOf(i));
+            i++;
+        }*/
+
 }
